fad768ea5f4287b3e4b3fba85edc95fda9a30fae
alexeyturalysov <alexeyturalysov@gmail.com>
2025-01-22 22:12:04 +0900

    backend8000 16 static admin

--- UgostyMainService/UgostyMainService/settings.py
+++ UgostyMainService/UgostyMainService/settings.py
@@ -219,9 +219,12 @@ USE_TZ = True
 
 
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 import os
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
